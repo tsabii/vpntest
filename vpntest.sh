f93d40426b5220bdf136832acc47818b524c09ff
good cop bad cop <36274561+retrohun@users.noreply.github.com>
2019-04-05 16:40:55 +0000

    refuktorign
    without any additional bashism

--- vpntest.sh
+++ vpntest.sh
@@ -14,18 +14,7 @@ DBT=2  # Delay Between Tries
 # http://www.linuxjournal.com/content/validating-ip-address-bash-script
 function valid_ip()
 {
-    local  ip=$1
-    local  stat=1
-
-    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-        OIFS=$IFS
-        IFS='.'
-        ip=($ip)
-        IFS=$OIFS
-        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
-        stat=$?
-    fi
-    return $stat
+  [[ .${1:-} =~ ^(\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])){4}$ ]] || return 1
 }
 
 
